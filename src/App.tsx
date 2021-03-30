1b0b75dbf0a381cbf48070050fdee135f7e7dae0
Joseph Richardson <richardsonjp93@gmail.com>
2021-03-30 12:56:57 +0100

    item info displayed in a grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import {useState} from 'react';
 import {useQuery} from 'react-query';
+//Components
+import Item from './components/Item/Item';
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 import {Wrapper} from './App.styles';
+
 
 //create type to store items that go into cart with extra amount field
 export type CartItemType = {
@@ -25,13 +28,33 @@ const getProducts = async (): Promise<CartItemType[]> =>
 
 
 const App = () => {
+
   const {data, isLoading, error} = useQuery<CartItemType[]>(
     'products', 
     getProducts
   );
+
   console.log(data)
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+  if(isLoading) return <LinearProgress />;
+  if(error) return <div>Something went wrong...</div>
+
   return (
-    <div className="App">Start</div>
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
   );
 }
 
