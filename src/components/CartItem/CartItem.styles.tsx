14f2f156033fbc753b83792f8a9e8c6ab5b528a9
Joseph Richardson <richardsonjp93@gmail.com>
2021-03-31 16:16:12 +0100

    added top border to cart items

--- src/components/CartItem/CartItem.styles.tsx
+++ src/components/CartItem/CartItem.styles.tsx
@@ -4,6 +4,7 @@ export const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   font-family: Arial, Helvetica, sans-serif;
+  border-top: 1px solid lightblue;
   border-bottom: 1px solid lightblue;
   padding-bottom: 20px;
 
