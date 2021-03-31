fc0fd1b686bf8117cd675a7404ae0736cb6fc4e9
Joseph Richardson <richardsonjp93@gmail.com>
2021-03-31 15:24:23 +0100

    styling for cartItems

--- src/components/CartItem/CartItem.tsx
+++ src/components/CartItem/CartItem.tsx
@@ -26,6 +26,7 @@ const CartItem: React.FC<Props> = ({item, addToCart, removeFromCart}) => (
           >
             -
           </Button>
+          <p>{item.amount}</p>
           <Button
           size="small"
           disableElevation
